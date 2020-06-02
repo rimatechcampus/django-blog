5472b3cf3a435451322ae0b30263b12166499341
Julia <riyamtechcampus@gmail.com>
2020-06-03 00:40:35 +0300

    make first posts

--- vblog/views.py
+++ vblog/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+posts = [
+    {
+        'title':'post one',
+        'content':'python is easy and fun',
+        'post_date':'2-6-2020',
+        'author':'rima hazim'
+    },
+    {
+        'title':'post two',
+        'content':'django is easy and fun',
+        'post_date':'2-6-2020',
+        'author':'rmrm hazim'
+    },
+    {
+        'title':'post three',
+        'content':'web developemnt  is easy and fun',
+        'post_date':'2-6-2020',
+        'author':'rora hazim'
+    }
+]
 
 def home(request):
-    return render(request , 'home.html', {})
+    context = {
+        'title':'home',
+        'posts' : posts
+    }
+    return render(request , 'home.html', context)
