517c6bf7781c6c5cc4d517c88e088ebf280605a6
Julia <riyamtechcampus@gmail.com>
2020-06-03 13:31:11 +0300

    make register page in user app

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from .forms import RegisterForm
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+    else:
+        form = RegisterForm()
+
+    context = {'title': 'register', 'form': form}
+    return render(request, 'register.html', context)
