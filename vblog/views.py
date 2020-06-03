54751e0bf229fb918b6bf467ae4b498d20ec607e
Julia <riyamtechcampus@gmail.com>
2020-06-03 10:42:12 +0300

    add new comment form post_detail page

--- vblog/views.py
+++ vblog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
+from .forms import NewComment
 # Create your views here.
 # posts = [
 #     {
@@ -40,6 +41,12 @@ def about(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     comments = post.comments.filter(active=True)
-    context = {'title': post, 'post': post, 'comments': comments}
+    comment_form = NewComment()
+    context = {
+        'title': post,
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form
+    }
 
     return render(request, 'post_detail.html', context)
