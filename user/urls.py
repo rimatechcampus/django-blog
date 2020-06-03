ddbe6b609e0e3311c28bf380f307406da9d83b9f
Julia <riyamtechcampus@gmail.com>
2020-06-03 19:31:07 +0300

    add login page  - activate it by loginView

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    # path('login/', views.login, name='login'),
+    path('login/', LoginView.as_view(template_name='login.html'),
+         name='login'),
+    path('logout/',
+         LogoutView.as_view(template_name='logout.html'),
+         name='logout'),
 ]
