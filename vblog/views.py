f49110c3bcf7f6bd08f35034e98d1175ad890a51
Julia <riyamtechcampus@gmail.com>
2020-06-03 08:21:46 +0300

    add posts from admin panel

--- vblog/views.py
+++ vblog/views.py
@@ -1,39 +1,34 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
-posts = [
-    {
-        'title':'post one',
-        'content':'python is easy and fun',
-        'post_date':'2-6-2020',
-        'author':'rima hazim'
-    },
-    {
-        'title':'post two',
-        'content':'django is easy and fun',
-        'post_date':'2-6-2020',
-        'author':'rmrm hazim'
-    },
-    {
-        'title':'post three',
-        'content':'web developemnt  is easy and fun',
-        'post_date':'2-6-2020',
-        'author':'rora hazim'
-    }
-]
+# posts = [
+#     {
+#         'title':'post one',
+#         'content':'python is easy and fun',
+#         'post_date':'2-6-2020',
+#         'author':'rima hazim'
+#     },
+#     {
+#         'title':'post two',
+#         'content':'django is easy and fun',
+#         'post_date':'2-6-2020',
+#         'author':'rmrm hazim'
+#     },
+#     {
+#         'title':'post three',
+#         'content':'web developemnt  is easy and fun',
+#         'post_date':'2-6-2020',
+#         'author':'rora hazim'
+#     }
+# ]
+
 
 def home(request):
-    context = {
-        'title':'home',
-        'posts' : posts
-    }
-    return render(request , 'home.html', context)
-
-
+    posts = Post.objects.all()
+    context = {'title': 'home', 'posts': posts}
+    return render(request, 'home.html', context)
 
 
 def about(request):
-    context = {
-        'title': 'about'
-    }
-    return render(request , 'about.html', context)
+    context = {'title': 'about'}
+    return render(request, 'about.html', context)
